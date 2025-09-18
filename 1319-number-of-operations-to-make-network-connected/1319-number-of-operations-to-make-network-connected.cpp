8c2d38d0469e6245ec467478c13e7bc35d88cf4a
Navneeta <133473734+Navneetanavie@users.noreply.github.com>
2025-09-18 21:48:07 +0530

    Time: 27 ms (29.22%), Space: 46.8 MB (39.59%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-void dfs(int node, vector<vector<int>>&adj,vector<int>&vis){
-    vis[node]=1;
-    for(auto &it:adj[node]){
-         if(!vis[it])
-        dfs(it,adj,vis);
+  void make(int i,vector<int>&parent,vector<int>&size){
+      parent[i]=i;
+      size[i]=i;
+  }
+  void Union(int nodeA, int nodeB,vector<int>&parent,vector<int>&size){
+    nodeA=find(nodeA,parent);
+    nodeB=find(nodeB,parent);
+    if(nodeA!=nodeB){
+        if(size[nodeA]<size[nodeB]){
+            swap(nodeA,nodeB);
+        }
+        parent[nodeB]=nodeA;
+        size[nodeA]+=size[nodeB];
     }
-    
-}
+  }
+  int find(int node,vector<int>&parent){
+       if(node==parent[node]) return node;
+       return parent[node]=find(parent[node],parent);
+  }
+  
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int wire=connections.size();
-        if(wire<(n-1)) return -1;
-        vector<vector<int>>adj(n);
-        for(auto &it:connections){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
-        int cnt=0;
-        vector<int>vis(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-                cnt++;
-            }
-        }
-        return cnt-1;
+        if(connections.size()<n-1) return -1;
+        vector<int>parent(n),size(n);
+        for(int i=0;i<n;i++) make(i,parent,size);
+        for(auto it:connections)
+         Union(it[0],it[1],parent,size);
+         int cnt=0;
+         for(int i=0;i<n;i++){
+            if(parent[i]==i) cnt++;
+         }
+         return cnt-1;
     }
 };
