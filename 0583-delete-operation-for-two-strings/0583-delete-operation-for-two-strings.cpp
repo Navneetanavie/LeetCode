666370fdd38cb0988034ab06d93207c08cb778dc
Navneeta <133473734+Navneetanavie@users.noreply.github.com>
2025-10-13 19:39:12 +0530

    Time: 13 ms (45.01%), Space: 16.7 MB (71.73%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-vector<vector<int>>memo;
-    int helper(string& word1, string& word2, int m, int n){
+vector<vector<int>>dp;
+    int lcs(string& word1, string& word2, int m, int n){
         if(m == 0 || n == 0) return 0;
-        if(memo[m][n] != -1) return memo[m][n];
+        if(dp[m][n] != -1) return dp[m][n];
         
-        if(word1[m-1]==word2[n-1]) return memo[m][n] = 1 + helper(word1, word2, m-1, n-1);
-        else return memo[m][n] = max(helper(word1, word2, m-1, n),helper(word1, word2, m, n-1));
+        if(word1[m-1]==word2[n-1]) return dp[m][n] = 1 + lcs(word1, word2, m-1, n-1);
+        else return dp[m][n] = max(lcs(word1, word2, m-1, n),lcs(word1, word2, m, n-1));
     }
     int minDistance(string word1, string word2) {
         int m=word1.size();
         int n=word2.size();
-       memo=vector<vector<int>>(m+1, vector<int>(n+1, -1));
-        int CommonChar=helper(word1,word2,m,n);
+       dp=vector<vector<int>>(m+1, vector<int>(n+1, -1));
+        int CommonChar=lcs(word1,word2,m,n);
         int steps=(m-CommonChar)+(n-CommonChar);
         return steps;
 
