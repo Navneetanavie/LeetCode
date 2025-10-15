fffa0abff882bdcf9360c3cf45951ae6da20623d
Navneeta <133473734+Navneetanavie@users.noreply.github.com>
2025-10-15 13:05:53 +0530

    Time: 0 ms (100%), Space: 17.2 MB (18.74%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,42 +3,47 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>>vis(m,vector<int>(n,0));
+        queue<pair<int,int>>q;
+        vector<vector<int>>vis=grid;
+        int countFreshOranges=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    vis[i][j]=2;
-                   q.push({{i,j},0});
-                }
+                if(vis[i][j]==2)
+                   q.push({i,j});
+                if(vis[i][j]==1)
+                   countFreshOranges++;
                  
             }
         }
 
-        int maxTm=0;
+        int minutes=-1;
         int dr[]={0,1,0,-1};
         int dc[]={1,0,-1,0};
         while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int tm=q.front().second;
+            int size=q.size();
+            while(size--){
+            int r=q.front().first;
+            int c=q.front().second;
+            
             q.pop();
-            maxTm=max(tm,maxTm);
+            
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                if(nr>=0&&nr<m&&nc>=0&&nc<n&&!vis[nr][nc]&&grid[nr][nc]!=0){
+                if(nr>=0&&nr<m&&nc>=0&&nc<n&&vis[nr][nc]==1){
                          vis[nr][nc]=2;
-                         q.push({{nr,nc},tm+1});
+                         countFreshOranges--;
+                         q.push({nr,nc});
                 }
+             }
             }
+            minutes++;
         }
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(grid[i][j]==1&&!vis[i][j])
-            return -1;
-        }
-       }
-      return maxTm;
+       
+           
+       
+       if(countFreshOranges>0)  return -1;
+         if(minutes==-1) return 0;
+       return minutes;
     }
 };
