9c19484aa444e9aac1ce15899b025278c4108e11
Navneeta <133473734+Navneetanavie@users.noreply.github.com>
2025-10-18 14:41:24 +0530

    Time: 13 ms (66.18%), Space: 46.6 MB (54.99%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-  void make(int i,vector<int>&parent,vector<int>&size){
-      parent[i]=i;
-      size[i]=i;
+vector<int>parent;
+vector<int>size;
+  int find(int node){
+    if(node==parent[node]) return node;
+    return parent[node]=find(parent[node]);
   }
-  void Union(int nodeA, int nodeB,vector<int>&parent,vector<int>&size){
-    nodeA=find(nodeA,parent);
-    nodeB=find(nodeB,parent);
+
+void Union(int nodeA, int nodeB){
+    nodeA=find(nodeA);
+    nodeB=find(nodeB);
     if(nodeA!=nodeB){
-        if(size[nodeA]<size[nodeB]){
-            swap(nodeA,nodeB);
-        }
-        parent[nodeB]=nodeA;
-        size[nodeA]+=size[nodeB];
+        if(size[nodeA]<size[nodeB])
+          swap(nodeA,nodeB);
+          parent[nodeB]=nodeA;
+          size[nodeA]+=size[nodeB];
     }
-  }
-  int find(int node,vector<int>&parent){
-       if(node==parent[node]) return node;
-       return parent[node]=find(parent[node],parent);
-  }
-  
+}
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size()<n-1) return -1;
-        vector<int>parent(n),size(n);
-        for(int i=0;i<n;i++) make(i,parent,size);
-        for(auto it:connections)
-         Union(it[0],it[1],parent,size);
-         int cnt=0;
-         for(int i=0;i<n;i++){
-            if(parent[i]==i) cnt++;
-         }
-         return cnt-1;
+        parent.resize(n);
+        size.resize(n);
+        for(int i=0;i<n;i++){
+         parent[i]=i;
+         size[i]=i;
+        }
+     for(auto it:connections){
+         Union(it[0],it[1]);
+     }
+     int cnt=0;
+     for(int i=0;i<n;i++){
+        if(parent[i]==i) cnt++;
+     }
+        return cnt-1;
     }
 };
