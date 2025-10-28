e5ca964871c21aad35dc3c8f2cc64f2f8bbe5769
Navneeta <133473734+Navneetanavie@users.noreply.github.com>
2025-10-28 11:54:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        // Min-heap using lambda comparator on squared distance
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-        
-        for (auto& point : points) {
-            int distSq = point[0] * point[0] + point[1] * point[1];
-            pq.push({distSq, {point[0], point[1]}});
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        for(auto &it:points){
+            int dis=sqrt((it[0]*it[0])+(it[1]*it[1]));
+            pq.push({dis,{it[0],it[1]}});
         }
-
-        vector<vector<int>> result;
-        while (k--) {
-            auto [dist, coord] = pq.top(); pq.pop();
-            result.push_back({coord.first, coord.second});
+          vector<vector<int>>ans;
+        while(k--){
+            auto [dist,coord]=pq.top();
+             pq.pop();
+          ans.push_back({coord.first,coord.second});
+         
+          
         }
-        return result;
+        return ans;
     }
 };
